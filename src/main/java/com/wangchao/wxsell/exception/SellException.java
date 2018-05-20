0af7fa1d98ee2bb37e1f830350b01b5e81af23f7
wangchao <244067166@qq.com>
2018-05-20 22:33:35 +0800

    微信点餐

--- src/main/java/com/wangchao/wxsell/exception/SellException.java
+++ src/main/java/com/wangchao/wxsell/exception/SellException.java
@@ -1,4 +1,15 @@
 package com.wangchao.wxsell.exception;
 
-public class SellException {
+import com.wangchao.wxsell.enums.ResultEnum;
+import lombok.Data;
+
+@Data
+public class SellException extends RuntimeException{
+
+    private Integer code;
+
+    public SellException(ResultEnum resultEnum) {
+        super(resultEnum.getMessage());
+        this.code=resultEnum.getCode();
+    }
 }
